7c07825bedb1f94a6dae5c79c1506a70b4948618
MingY-TSAI <78520270+MingY-TSAI@users.noreply.github.com>
2021-02-10 20:12:31 +0800

    Update mongodb.py

--- mongodb.py
+++ mongodb.py
@@ -16,7 +16,7 @@ def constructor():
 #----------------------------儲存使用者的股票--------------------------
 def write_user_stock_fountion(stock, bs, price):  
     db=constructor()
-    collect = db['mystock']
+    collect = db['stockdb']
     collect.insert({"stock": stock,
                     "data": 'care_stock',
                     "bs": bs,
@@ -27,13 +27,13 @@ def write_user_stock_fountion(stock, bs, price):
 #----------------------------殺掉使用者的股票--------------------------
 def delete_user_stock_fountion(stock):  
     db=constructor()
-    collect = db['mystock']
+    collect = db['stockdb']
     collect.remove({"stock": stock})
     
 #----------------------------秀出使用者的股票--------------------------
 def show_user_stock_fountion():  
     db=constructor()
-    collect = db['mystock']
+    collect = db['stockdb']
     cel=list(collect.find({"data": 'care_stock'}))
 
     return cel
