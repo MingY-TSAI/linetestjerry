8a8581b183c7112a135bfd8278b91ec8e0862392
MingY-TSAI <78520270+MingY-TSAI@users.noreply.github.com>
2021-02-10 18:25:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -60,11 +60,11 @@ def handle_message(event):
         return 0
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
 
-#主程式
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port,debug=False)
+# #主程式
+# import os
+# if __name__ == "__main__":
+#     port = int(os.environ.get('PORT', 5000))
+#     app.run(host='0.0.0.0', port=port,debug=True)
