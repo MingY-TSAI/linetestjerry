a004ee2b9b95b5a06d18f4d9026e59dff0490d55
MingY-TSAI <78520270+MingY-TSAI@users.noreply.github.com>
2021-02-10 20:50:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -41,12 +41,14 @@ def callback():
     return 'OK'
 
 
+
 #訊息傳遞區塊
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     ### 抓到顧客的資料 ###
-    profile = line_bot_api.get_profile(event.source.user_id)
-    uid = profile.user_id #使用者ID
+#     profile = line_bot_api.get_profile(event.source.user_id)
+#     uid = profile.user_id #使用者ID
+    uid = event.source.user_id
     usespeak=str(event.message.text) #使用者講的話
     if re.match('[0-9]{4}[<>][0-9]',usespeak): # 先判斷是否是使用者要用來存股票的
         mongodb.write_user_stock_fountion(stock=usespeak[0:4], bs=usespeak[4:5], price=usespeak[5:])
@@ -58,6 +60,8 @@ def handle_message(event):
         mongodb.delete_user_stock_fountion(stock=usespeak[2:])
         line_bot_api.push_message(uid, TextSendMessage(usespeak+'已經刪除成功'))
         return 0
+    else:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage('輸入錯誤'))
 
 
 if __name__ == '__main__':
