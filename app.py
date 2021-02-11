e168adb8d4790aea78578cf8d83676af1f243c2d
MingY-TSAI <78520270+MingY-TSAI@users.noreply.github.com>
2021-02-11 19:00:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-
+#載入LineBot所需要的套件
 from flask import Flask, request, abort
-from linebot import (LineBotApi, WebhookHandler)
-from linebot.exceptions import (InvalidSignatureError)
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
 from linebot.models import *
-# import mongodb
-import re
-from pymongo import MongoClient
-import urllib.parse
-import datetime
 
 app = Flask(__name__)
 
@@ -33,56 +33,17 @@ def callback():
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+
     return 'OK'
-
-
-# Authentication Database認證資料庫
-Authdb='stockdb-abcd1234'
-
-
-
-
-
-
 
 #訊息傳遞區塊
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    ### 抓到顧客的資料 ###
-    profile = line_bot_api.get_profile(event.source.user_id)
-    uid = profile.user_id #使用者ID
-    usespeak=str(event.message.text) #使用者講的話
-    line_bot_api.push_message(uid, TextSendMessage(usespeak[0:4]+'已經儲存成功'))
-#     if re.match('[0-9]{4}[<>][0-9]',usespeak) is not None: # 先判斷是否是使用者要用來存股票的
-#         stock=usespeak[0:4]
-#         bs=usespeak[4:5]
-#         price=usespeak[5:]
-#         client = MongoClient("mongodb+srv://Jerry:abcd1234@cluster0.z7sx8.mongodb.net/stockdb?retryWrites=true&w=majority")
-#         db = client['stockdb-abcd1234']
-#         collect = db['mystock']
-#         collect.insert({"stock": stock,
-#                         data": 'care_stock',
-#                         "bs": bs,
-#                         "price": float(price),
-#                         "date_info": datetime.datetime.utcnow()
-#                         })
-#         line_bot_api.push_message(uid, TextSendMessage(usespeak[0:4]+'已經儲存成功'))
-#         return 0
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token,message)
 
-    
-#     elif re.match('刪除[0-9]{4}',usespeak) is not None: # 刪除存在資料庫裡面的股票
-#         stock=usespeak[2:]
-#         client = MongoClient("mongodb+srv://Jerry:abcd1234@cluster0.z7sx8.mongodb.net/stockdb?retryWrites=true&w=majority")
-#         db = client['stockdb-abcd1234']
-#         collect = db['mystock']
-#         collect.remove({"stock": stock}) 
-#         line_bot_api.push_message(uid, TextSendMessage(usespeak+'已經刪除成功'))
-#         return 0
-    
-#     else:
-#         line_bot_api.push_message(uid, TextSendMessage('輸入代碼錯誤'))
-        return 0
-
-
-if __name__ == '__main__':
-    app.run(debug=False)
+#主程式
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
