9f1424c35a711f0894013bb143c37b1da70d5042
MingY-TSAI <78520270+MingY-TSAI@users.noreply.github.com>
2021-02-11 19:57:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,11 +43,11 @@ def callback():
 
     return 'OK'
 
-#訊息傳遞區塊
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+# #訊息傳遞區塊
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     message = TextSendMessage(text=event.message.text)
+#     line_bot_api.reply_message(event.reply_token,message)
 
 #訊息傳遞區塊
 @handler.add(MessageEvent, message=TextMessage)
