1ecae9c15ec85940e7faf914b51797cc4308b6a0
MingY-TSAI <78520270+MingY-TSAI@users.noreply.github.com>
2021-02-11 19:56:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,15 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
+ #載入LineBot所需要的套件
+# import mongodb
+import re
+from pymongo import MongoClient
+import urllib.parse
+import datetime
 
 app = Flask(__name__)
+
 
 # 必須放上自己的Channel Access Token
 line_bot_api = LineBotApi('OC/2LXxWpqBrf+PiU4+ALXildS+3uZCvMbYnE7bfr3MvjNx4p9K7xGZwOQItMie9IFyCRHs79f7IXz2ffyLHK1fGgfTuM9IZn3KEuLCuL0Ovyx6k/HwAS9N1RxFi3GLiX5HUfM1K83aP/czfPW4zIAdB04t89/1O/w1cDnyilFU=')
@@ -42,8 +49,52 @@ def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token,message)
 
+#訊息傳遞區塊
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    ### 抓到顧客的資料 ###
+    profile = line_bot_api.get_profile(event.source.user_id)
+    uid = profile.user_id #使用者ID
+    usespeak=str(event.message.text) #使用者講的話
+    line_bot_api.push_message(TextSendMessage( str(uid)+usespeak))#測試回復    
+    if re.match('[0-9]{4}[<>][0-9]',usespeak) is not None:# 先判斷是否是使用者要用來存股票的
+        stock=usespeak[0:4] 
+        bs=usespeak[4:5] 
+        price=usespeak[5:]
+        
+        client = MongoClient("mongodb+srv://Jerry:abcd1234@cluster0.z7sx8.mongodb.net/stockdb?retryWrites=true&w=majority")
+        db = client['stockdb-abcd1234']
+        collect = db['mystock']
+        collect.insert({"stock": stock,
+                        "data": 'care_stock',
+                        "bs": bs,
+                        "price": float(price),
+                        "date_info": datetime.datetime.utcnow()
+                       })
+        line_bot_api.reply_message(TextSendMessage(usespeak[0:4]+'已經儲存成功'))
+        return 0
+
+  
+    elif re.match('刪除[0-9]{4}',usespeak) is not None: # 刪除存在資料庫裡面的股票
+        stock=usespeak[2:]
+        client = MongoClient("mongodb+srv://Jerry:abcd1234@cluster0.z7sx8.mongodb.net/stockdb?retryWrites=true&w=majority")
+        db = client['stockdb-abcd1234']    
+        collect = db['mystock']
+        collect.remove({"stock": stock})            
+        line_bot_api.push_message(uid, TextSendMessage(usespeak+'已經刪除成功'))
+        return 0
+    else:
+        line_bot_api.push_message(TextSendMessage(usespeak+'輸入錯誤'))
+        return 0
+    
+    
+    
 #主程式
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)  
+    
