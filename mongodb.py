abdd2f7f1462469014c5b1807e24573465af93d7
MingY-TSAI <78520270+MingY-TSAI@users.noreply.github.com>
2021-02-12 16:12:54 +0800

    Update mongodb.py

--- mongodb.py
+++ mongodb.py
@@ -2,24 +2,32 @@
 from pymongo import MongoClient
 import urllib.parse
 import datetime
+from flask import Flask
+from flask_pymongo import pymongo
+from app import app
 ###############################################################################
 #                       股票機器人 Python基礎教學 【pymongo教學】                      #
 ###############################################################################
 
 # Authentication Database認證資料庫
-Authdb='stockdb-abcd1234'
+Authdb='stockdb'
+
+
+
+
 
 ##### 資料庫連接 #####
 def constructor():
     #client = MongoClient('你的連接指令')
-    client = MongoClient("mongodb+srv://Jerry:abcd1234@cluster0.z7sx8.mongodb.net/stockdb?retryWrites=true&w=majority")
-    db = client[Authdb]
+    CONNECTION_STRING = "mongodb+srv://Jerry:abcd1234@cluster0.3gbxu.mongodb.net/stockdb?retryWrites=true&w=majority"
+    client = pymongo.MongoClient(CONNECTION_STRING)
+    db = client.get_database('flask_mongodb_atlas')
     return db
    
 #----------------------------儲存使用者的股票--------------------------
 def write_user_stock_fountion(stock, bs, price):  
     db=constructor()
-    collect = db['mystock']
+    collect = pymongo.collection.Collection(db, 'mystock')
     collect.insert({"stock": stock,
                     "data": 'care_stock',
                     "bs": bs,
@@ -30,13 +38,13 @@ def write_user_stock_fountion(stock, bs, price):
 #----------------------------殺掉使用者的股票--------------------------
 def delete_user_stock_fountion(stock):  
     db=constructor()
-    collect = db['mystock']
+    collect = pymongo.collection.Collection(db, 'mystock')
     collect.remove({"stock": stock})
     
 #----------------------------秀出使用者的股票--------------------------
 def show_user_stock_fountion():  
     db=constructor()
-    collect = db['mystock']
+    collect = pymongo.collection.Collection(db, 'mystock')
     cel=list(collect.find({"data": 'care_stock'}))
 
     return cel
