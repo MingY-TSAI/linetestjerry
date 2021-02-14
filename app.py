d4006d926fa3a4d2d3dbae2b6c8391454ad38197
MingY-TSAI <78520270+MingY-TSAI@users.noreply.github.com>
2021-02-14 23:31:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def handle_message(event):
 #######本月至昨日標準差分析
         yes = datetime.datetime.now()- datetime.timedelta(days = 1)
         print(yes.strftime("%Y%m%d"))
-        yes = '20210205'
+        yes = '20210205'######################################################################################################################記得改
         url='https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date={0}&stockNo=2330'.format(yes)
 
         list_req = requests.get(url)
