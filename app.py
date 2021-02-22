bb69ef7c62f67584fee4bb4e87d10d73740b0035
MingY-TSAI <78520270+MingY-TSAI@users.noreply.github.com>
2021-02-22 10:25:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -369,35 +369,36 @@ def handle_message(event):
             line_bot_api.push_message(uid, ImageSendMessage(original_content_url=image_url, preview_image_url=image_url))
         return 0
 
-    elif re.match('買啥',usespeak) is not None:
-        elected='' # 最後可以買的股票放這裡
-        ########## 當短期5日線突破20日線 ##########
-        url = 'https://tw.screener.finance.yahoo.net/screener/ws?PickID=100205&PickCount=1700&f=j'
-        list_req = requests.get(url)
-        soup = BeautifulSoup(list_req.content, "html.parser")
-        getjson1=json.loads(soup.text)
+#     elif re.match('買啥',usespeak) is not None:
+#         elected='' # 最後可以買的股票放這裡
+#         ########## 當短期5日線突破20日線 ##########
+#         url = 'https://tw.screener.finance.yahoo.net/screener/ws?PickID=100205&PickCount=1700&f=j'
+#         list_req = requests.get(url)
+#         soup = BeautifulSoup(list_req.content, "html.parser")
+#         getjson1=json.loads(soup.text)
 
-        ########## 股本大於20億 ##########
-        url = 'https://tw.screener.finance.yahoo.net/screener/ws?PickID=100538,100539,100540,100541&PickCount=1700&f=j&366'
-        list_req = requests.get(url)
-        soup = BeautifulSoup(list_req.content, "html.parser")
-        getjson2=json.loads(soup.text)
+#         ########## 股本大於20億 ##########
+#         url = 'https://tw.screener.finance.yahoo.net/screener/ws?PickID=100538,100539,100540,100541&PickCount=1700&f=j&366'
+#         list_req = requests.get(url)
+#         soup = BeautifulSoup(list_req.content, "html.parser")
+#         getjson2=json.loads(soup.text)
 
-        for i in getjson1['items']:
-            if i['symid'] in str(getjson2['items']):
-        ########## 週均量大於 1000 張 ##########   
-                url = 'https://tw.stock.yahoo.com/q/q?s=' + i['symid']
-                getjson3=pd.read_html(url,encoding='big5',header=0)
+#         for i in getjson1['items']:
+#             if i['symid'] in str(getjson2['items']):
+#         ########## 週均量大於 1000 張 ##########   
+#                 url = 'https://tw.stock.yahoo.com/q/q?s=' + i['symid']
+#                 getjson3=pd.read_html(url,encoding='big5',header=0)
 
-                if getjson3[2]['張數'].values[0] >1000 :
-                    elected = elected + i['symid'] +'\t' +i['symname']+'\t' +str(getjson3[2]['成交'].values[0])+'\n'
+#                 if getjson3[2]['張數'].values[0] >1000 :
+#                     elected = elected + i['symid'] +'\t' +i['symname']+'\t' +str(getjson3[2]['成交'].values[0])+'\n'
 
-        ########## 秀出結果 ##########            
-        if elected != '':# 判斷是不是空直
-            print(elected)
-            line_bot_api.push_message('U5594160c067df9c2d9b4ceb12b0d90ad', TextSendMessage(elected))
-        else:
-            line_bot_api.push_message('U5594160c067df9c2d9b4ceb12b0d90ad', TextSendMessage('沒有股票可以買'))
+#         ########## 秀出結果 ##########            
+#         if elected != '':# 判斷是不是空直
+#             line_bot_api.push_message('U5594160c067df9c2d9b4ceb12b0d90ad', TextSendMessage(elected))
+#             return 0
+#         else:
+#             line_bot_api.push_message('U5594160c067df9c2d9b4ceb12b0d90ad', TextSendMessage('沒有股票可以買'))
+#             return 0
     
     
     else:
